960860d2d7ca0bf4fd1c40792946c7c5e677916d
1101321 <hcri0909@gmail.com>
2022-02-04 19:07:26 +0900

    [Finish] Java chapter4

---
+++ JavaWcp/src/Chapter04.java
@@ -0,0 +1,12 @@
+
+public class Chapter04 {
+	public static void main(String[] args) {
+		String str = "田中　太郎";
+		System.out.println(str);
+		str = "田中　次郎";
+		System.out.println(str);
+		
+		final int LUCKY_NUMBER = 7;
+		System.out.println(LUCKY_NUMBER);
+	}
+}
