03a6c20d3d03cbeab8611a84357e5d6f51bab650
1101321 <hcri0909@gmail.com>
2022-02-04 19:50:16 +0900

    [Finish] Java chapter5

---
+++ JavaWcp/src/Chapter05.java
@@ -0,0 +1,21 @@
+
+public class Chapter05 {
+	public static void main (String[] args) {
+		long a = 111111;
+		System.out.println(a);
+		double b = 0.1;
+		System.out.println(b);
+		boolean c = true;
+		System.out.println(c);
+		
+		Float d = 0.2f;
+		System.out.println(d);
+		Integer e = 222222;
+		System.out.println(e);
+		String f = "あ";
+		System.out.println(f);
+		
+		int[] g = { 1, 2, 3, 4, 5 };
+		System.out.println(g[3]);
+	}
+}
